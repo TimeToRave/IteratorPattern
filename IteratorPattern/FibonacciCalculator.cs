273bc3509abd4a6a30d2588660223715d29b47a6
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-12 01:18:53 +0300

    Добавлен класс - для получения экземпляра необходимого итератора

--- IteratorPattern/FibonacciCalculator.cs
+++ IteratorPattern/FibonacciCalculator.cs
@@ -1,7 +1,20 @@
-﻿namespace IteratorPattern
+﻿using System.Collections.Generic;
+
+namespace IteratorPattern
 {
     public class FibonacciCalculator
     {
-        
+        public FibonacciIterator GetFibonacciIterator(string order, int initial, int steps)
+        {
+                
+            Dictionary<string, FibonacciIterator> availableMethods = new Dictionary<string, FibonacciIterator>()
+            {
+                {"ASC", new FibonacciAscIterator(initial, steps)},
+                {"DESC", new FibonacciDescIterator(initial, steps)}
+            };
+                
+            var iterator = availableMethods[order];
+            return iterator;
+        }
     }
 }
