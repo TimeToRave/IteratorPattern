78a41db203c2fd0b8a6176869994fcaece773a52
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-12 01:17:37 +0300

    Реализация общего класса - итератора для подсчета чисел Фибоначчи

--- IteratorPattern/FibonacciIterator.cs
+++ IteratorPattern/FibonacciIterator.cs
@@ -1,7 +1,19 @@
-﻿namespace IteratorPattern
+﻿using System;
+
+namespace IteratorPattern
 {
-    public class FibonacciIterator
+    public abstract class  FibonacciIterator
     {
+        protected int StepCount { get; set; }
+        protected int Position { get; set; }
+
+        public abstract bool MoveNext();
         
+        public int GetCurrent()
+        {
+            double sqrt5 = Math.Sqrt(5);
+            double phi = (sqrt5 + 1) / 2;
+            return Convert.ToUInt16(1 / sqrt5 * Math.Pow(phi, Position));
+        }
     }
 }
