37b738fa6838e81a726ccb41e12037ef88223509
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-13 00:09:02 +0300

    Реализована программа, которая вычисляет последовательность Фибоначчи заданной длины и записывает ее в файл

---
+++ IteratorPattern/FileOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace IteratorPattern
+{
+    /// <summary>
+    /// Класс работы с файловой системой
+    /// </summary>
+    public class FileOperator
+    {
+        /// <summary>
+        /// Записывает строку в файл
+        /// </summary>
+        /// <param name="filePath">Адрес файла</param>
+        /// <param name="content">Записыаемое содержимое
+        /// </param>
+        public void WriteTextToFile(string filePath, string content)
+        {
+            try
+            {
+                File.WriteAllText(filePath, content);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"{filePath} file doesn't exists");
+            }
+        }
+    }
+}
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace IteratorPattern
+{
+    static class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int stepsCount = 10;
+            int initialStep = 0;
+            
+            // Получает количество шагов
+            var argumentsIsValid = args.Length > 0  ? int.TryParse(args[0], out stepsCount) : false;
+            
+            // Получает начальный шаг, с которого будут выполняться вычисления
+            argumentsIsValid = args.Length > 1 && int.TryParse(args[1], out initialStep);
+            // Получает название файла, в который будет производиться запись
+            string outputFilePath = (args.Length > 2 && args[2].Length > 0) ? args[2] : "output.txt";
+            
+            var calculator = new FibonacciCalculator();
+            var iterator = calculator.GetFibonacciIterator(initialStep, stepsCount);
+
+            StringBuilder fibonacciSequence = new StringBuilder();
+            while (iterator.MoveNext())
+            {
+                fibonacciSequence.AppendLine(iterator.GetCurrent().ToString());
+            }
+
+            FileOperator fileOperator = new FileOperator();
+            fileOperator.WriteTextToFile(outputFilePath, fibonacciSequence.ToString());
+            Console.ReadLine();
+        }
+    }
+}
