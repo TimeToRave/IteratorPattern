ceef45685d6d824e88197b76abf4820f51769a64
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-12 23:08:08 +0300

    Выделение метода для определения направления формирования последовательности

--- IteratorPattern/FibonacciCalculator.cs
+++ IteratorPattern/FibonacciCalculator.cs
@@ -9,21 +9,21 @@ namespace IteratorPattern
         /// Получает наиболее подходящий итератор
         /// для формирования последовательноти Фибоначчи
         /// </summary>
-        /// <param name="order">Направление формирования последовательности</param>
         /// <param name="initial">Начальный шаг, с которого будет строиться последовательность</param>
         /// <param name="steps">Количество шагов, которые будут выполнены итератором</param>
         /// <returns>Итератор</returns>
-        public FibonacciIterator GetFibonacciIterator(string order, int initial, int steps)
+        public FibonacciIterator GetFibonacciIterator(int initial, int steps)
         {
             Dictionary<string, FibonacciIterator> availableMethods = new Dictionary<string, FibonacciIterator>()
             {
                 {"ASC", new FibonacciAscIterator(initial, steps)},
-                {"DESC", new FibonacciDescIterator(initial, steps)}
+                {"DESC", new FibonacciDescIterator(initial, Math.Abs(steps))}
             };
 
+            string orderName = GetIteratorOrder(steps);
             try
             {
-                var iterator = availableMethods[order];
+                var iterator = availableMethods[orderName];
                 return iterator;
             }
             catch (Exception)
@@ -31,6 +31,23 @@ namespace IteratorPattern
                 return new FibonacciAscIterator(initial, steps);
             }
 
+        }
+
+        /// <summary>
+        /// Определяет направление формирования последовательсности Фибоначчи
+        /// Если количество шагов отрицательное, то производится обратный проход
+        /// </summary>
+        /// <param name="steps">Количество шагов</param>
+        /// <returns>Название метода прохода</returns>
+        private string GetIteratorOrder(int steps)
+        {
+            string order = "ASC";
+            if (steps  < 0)
+            {
+                order = "DESC";
+            }
+
+            return order;
         }
     }
 }
